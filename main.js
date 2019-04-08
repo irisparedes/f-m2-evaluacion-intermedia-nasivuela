073f9bb4fffcd574bd97d5fe2453ac0d909fc4d0
Nasiba Carrasco <nasivuela@gmail.com>
2019-04-08 18:39:28 +0200

    Add guide steps

---
+++ main.js
@@ -0,0 +1,32 @@
+// Un juego que genere un número aleatorio entre 1 y 100, oculto. La jugadora tiene que adivinarlo mediante un input y un botón. El juego le dará pistas (demasiado bajo, demasiado alto) para que pueda adivinarlo. E irá contando los intentos.
+
+// HTML
+// Un input number
+// Un botón
+// Un espacio para el feedback
+// Un espacio para el contador
+
+// Recoger elementos
+  // Input
+  // botón
+  // feedback
+  // contador
+
+// Generar un número aleatorio
+// Crear un contador a 0
+
+// Escuchar click de botón
+
+// Cuando se haga click en botón (handler)
+  // Recoger el número del input
+  
+  // Compararlo con el aleatorio y
+    // Si es igual
+      // Feedback has ganado
+    // Si el número del input es mayor
+      // Feedback demasiado alto
+    // Si el número del input es menor
+      // Feedback demasiado bajo
+
+  // Aumentar el contador y ponerlo en el elemento contador de HTML
+  
